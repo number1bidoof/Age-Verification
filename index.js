9f034939d4e615612ffb697fa4c88228d2a2790d
number1bidoof <anabdalla1@my.waketech.edu>
2025-01-31 12:20:12 -0500

    Finished Assigment

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ let age;
 MyButton.onclick = function(){
     age = Myage.value;
     age = Number(age);
-    if (age < 10) {
-        MyText.innerHTML = "your too young for this website"
-    } else  if (age == 100) {
+    if (age == 100) {
         MyText.innerHTML = "You can enter the website, but its your last year doing so"
-    } else if (age == 0) {
+    } else if (age <= 0 ){
         MyText.innerHTML = "How are you that YOUNG big bruh, your not even born yet anyways still not allowed to enter"
-    } else if (age > 100) {
+    } else if (age > 100 && age < 1000) {
         MyText.innerHTML = "Your too old for this website"
     } else if (age == 1000) {
-        MyButton.innerHTML = "How are you 1000 years old big bruh? is that even possible? STILL not allowed to enter this website "
+        MyText.innerHTML = "How are you 1000 years old big bruh? is that even possible? STILL not allowed to enter this website "
+    } else if (age < 18 && age >= 16) {
+        MyText.innerHTML = " You are really close to being able to access the website, but you are still too young :("
+    } else  if (age < 16) {
+        MyText.innerHTML = "You cannot enter the website :("
     } else {
-        MyText.innerHTML = "you can enter the website"
+        MyText.innerHTML = "You can enter the website :)"
     }
 }
