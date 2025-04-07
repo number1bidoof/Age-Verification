a2e42f944cdb1c0a49efb047f3159f664dbe2de7
AyahAbdalla <115049581+AyahAbdalla@users.noreply.github.com>
2025-04-06 22:19:45 -0400

    Made it funnier

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ MyButton.onclick = function(){
     age = Myage.value;
     age = Number(age);
     if (age == 100) {
-        MyText.innerHTML = "You can enter the website, but its your last year doing so"
+        MyText.innerHTML = "You can enter the website, but your about to die soon :\("
     } else if (age <= 0 ){
         MyText.innerHTML = "How are you that YOUNG big bruh, your not even born yet anyways still not allowed to enter"
     } else if (age > 100 && age < 1000) {
         MyText.innerHTML = "Your too old for this website"
-    } else if (age == 1000) {
-        MyText.innerHTML = "How are you 1000 years old big bruh? is that even possible? STILL not allowed to enter this website "
+    } else if (age >= 1000) {
+        MyText.innerHTML = "How are you older than 1000 years old big bruh? is that even possible? STILL not allowed to enter this website "
     } else if (age < 18 && age >= 16) {
         MyText.innerHTML = " You are really close to being able to access the website, but you are still too young :("
     } else  if (age < 16) {
